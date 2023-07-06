e97faf9f892f754e3e5dc0bf5d0354e97fc73677
emiliano arguello <emiarg12@gmail.com>
2023-07-05 21:36:40 -0300

    Modificaciones

---
+++ index.js
@@ -0,0 +1,27 @@
+class ProductManager {
+    constructor(){
+    this.product=[] 
+    }
+    
+    static id=0
+    addproduct(title, price, descripction, code, stock){
+        ProductManager.id++
+        this.product({title, price, description, code, stock, id:ProductManager.id});
+}
+getProduct(){
+    return this.product}
+getProductById(id){
+    if(!this.product.find((product) => product.id === id))
+    return 'not found'}
+    
+    else{
+        return this.products.find((product)=> product.id === id)
+    }
+    const product = new ProductManager()
+console.log(product.getProduct())
+product.addProduct('colchon', 130000, 'resortes', '1234', 5)
+product.addProduct('almohada', 30000, 'con interior de plumas', '1122', 8)
+product.addProduct('sabanas', 5000, 'tres mil hilos', '1357', 3)
+
+console.log(product.getProduct());
+console.log(product.getProductById());
